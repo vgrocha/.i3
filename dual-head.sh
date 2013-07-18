ce7b360c713068b40ca188cb2e3e420a57b84f1d
Vinicius@dell <vinicius.darocha@das.ufsc.br>
2013-07-18 09:14:53 -0300

    added scripts for changing external monitor position

---
+++ dual-head.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI1 --off --output LVDS1 --mode 1280x800 --pos 368x1080 --rotate normal --output TV1 --off --output VGA1 --mode 1920x1080 --pos 0x0 --rotate normal
---
+++ mono-head.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI1 --off --output LVDS1 --mode 1280x800 --pos 312x160 --rotate normal --output TV1 --off --output VGA1 --mode 1280x800 --pos 312x160 --rotate normal
